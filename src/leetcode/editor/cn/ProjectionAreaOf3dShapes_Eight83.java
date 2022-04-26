4b1cc8bf40d05b9eb73872f28c67e7dc4e4a4846
xuanguang <xuanguang@xuanguangdeMacBook-Pro.local>
2022-04-26 13:09:27 +0800

    20220426

---
+++ src/leetcode/editor/cn/ProjectionAreaOf3dShapes_Eight83.java
@@ -0,0 +1,104 @@
+//在 n x n 的网格 grid 中，我们放置了一些与 x，y，z 三轴对齐的 1 x 1 x 1 立方体。 
+//
+// 每个值 v = grid[i][j] 表示 v 个正方体叠放在单元格 (i, j) 上。 
+//
+// 现在，我们查看这些立方体在 xy 、yz 和 zx 平面上的投影。 
+//
+// 投影 就像影子，将 三维 形体映射到一个 二维 平面上。从顶部、前面和侧面看立方体时，我们会看到“影子”。 
+//
+// 返回 所有三个投影的总面积 。 
+//
+// 
+//
+// 
+// 
+//
+// 
+// 
+//
+// 
+// 
+//
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：[[1,2],[3,4]]
+//输出：17
+//解释：这里有该形体在三个轴对齐平面上的三个投影(“阴影部分”)。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：grid = [[2]]
+//输出：5
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：[[1,0],[0,2]]
+//输出：8
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == grid.length == grid[i].length 
+// 1 <= n <= 50 
+// 0 <= grid[i][j] <= 50 
+// 
+// Related Topics 几何 数组 数学 矩阵 👍 103 👎 0
+
+
+package leetcode.editor.cn;
+
+public class ProjectionAreaOf3dShapes_Eight83 {
+    public static void main(String[] args) {
+        Solution solution = new ProjectionAreaOf3dShapes_Eight83().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int projectionArea(int[][] grid) {
+            int length = grid.length;
+            int xy = 0;
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < length; j++) {
+                    if (grid[i][j] != 0) {
+                        xy++;
+                    }
+                }
+            }
+
+            int xz = 0;
+            for (int i = 0; i < length; i++) {
+                int max = 0;
+                for (int j = 0; j < length; j++) {
+                    max = Math.max(max, grid[i][j]);
+                }
+                xz += max;
+            }
+
+            int yz = 0;
+            for (int j = 0; j < length; j++) {
+                int max = 0;
+                for (int i = 0; i < length; i++) {
+                    max = Math.max(max, grid[i][j]);
+                }
+                yz += max;
+            }
+
+            return xy + xz + yz;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
