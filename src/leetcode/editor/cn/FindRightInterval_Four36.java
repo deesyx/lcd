fb5099c1ee1d2c0da0e1aefd324f58c428cafbb7
xuanguang <xuanguang@xuanguangdeMacBook-Pro.local>
2022-05-20 18:23:20 +0800

    20220520

---
+++ src/leetcode/editor/cn/FindRightInterval_Four36.java
@@ -0,0 +1,91 @@
+//给你一个区间数组 intervals ，其中 intervals[i] = [starti, endi] ，且每个 starti 都 不同 。 
+//
+// 区间 i 的 右侧区间 可以记作区间 j ，并满足 startj >= endi ，且 startj 最小化 。 
+//
+// 返回一个由每个区间 i 的 右侧区间 的最小起始位置组成的数组。如果某个区间 i 不存在对应的 右侧区间 ，则下标 i 处的值设为 -1 。 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[1,2]]
+//输出：[-1]
+//解释：集合中只有一个区间，所以输出-1。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[3,4],[2,3],[1,2]]
+//输出：[-1,0,1]
+//解释：对于 [3,4] ，没有满足条件的“右侧”区间。
+//对于 [2,3] ，区间[3,4]具有最小的“右”起点;
+//对于 [1,2] ，区间[2,3]具有最小的“右”起点。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：intervals = [[1,4],[2,3],[3,4]]
+//输出：[-1,2,-1]
+//解释：对于区间 [1,4] 和 [3,4] ，没有满足条件的“右侧”区间。
+//对于 [2,3] ，区间 [3,4] 有最小的“右”起点。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= intervals.length <= 2 * 10⁴ 
+// intervals[i].length == 2 
+// -10⁶ <= starti <= endi <= 10⁶ 
+// 每个间隔的起点都 不相同 
+// 
+// Related Topics 数组 二分查找 排序 👍 151 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FindRightInterval_Four36 {
+    public static void main(String[] args) {
+        Solution solution = new FindRightInterval_Four36().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] findRightInterval(int[][] intervals) {
+            int n = intervals.length;
+            int[][] start = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                start[i][0] = intervals[i][0];
+                start[i][1] = i;
+            }
+            Arrays.sort(start, Comparator.comparingInt(o -> o[0]));
+
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++) {
+                int left = 0;
+                int right = n - 1;
+                int ansIndex = -1;
+                int target = intervals[i][1];
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (start[mid][0] >= target) {
+                        ansIndex = start[mid][1]; // 尽量找小的
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                ans[i] = ansIndex;
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
