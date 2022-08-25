df0e8f5f9914d296e592bc6156efb0d39d36a394
xuanguang.hu <huxuanguang@hotmail.com>
2022-08-25 13:45:45 +0800

    20220825

---
+++ src/leetcode/editor/cn/StepByStepDirectionsFromABinaryTreeNodeToAnother_Two096.java
@@ -0,0 +1,127 @@
+//给你一棵 二叉树 的根节点 root ，这棵二叉树总共有 n 个节点。每个节点的值为 1 到 n 中的一个整数，且互不相同。给你一个整数 
+//startValue ，表示起点节点 s 的值，和另一个不同的整数 destValue ，表示终点节点 t 的值。 
+//
+// 请找到从节点 s 到节点 t 的 最短路径 ，并以字符串的形式返回每一步的方向。每一步用 大写 字母 'L' ，'R' 和 'U' 分别表示一种方向： 
+//
+// 
+// 'L' 表示从一个节点前往它的 左孩子 节点。 
+// 'R' 表示从一个节点前往它的 右孩子 节点。 
+// 'U' 表示从一个节点前往它的 父 节点。 
+// 
+//
+// 请你返回从 s 到 t 最短路径 每一步的方向。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+//输出："UURL"
+//解释：最短路径为：3 → 1 → 5 → 2 → 6 。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 输入：root = [2,1], startValue = 2, destValue = 1
+//输出："L"
+//解释：最短路径为：2 → 1 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目为 n 。 
+// 2 <= n <= 10⁵ 
+// 1 <= Node.val <= n 
+// 树中所有节点的值 互不相同 。 
+// 1 <= startValue, destValue <= n 
+// startValue != destValue 
+// 
+// Related Topics 树 深度优先搜索 字符串 二叉树 👍 42 👎 0
+
+
+package leetcode.editor.cn;
+
+public class StepByStepDirectionsFromABinaryTreeNodeToAnother_Two096 {
+    public static void main(String[] args) {
+        Solution solution = new StepByStepDirectionsFromABinaryTreeNodeToAnother_Two096().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public String getDirections(TreeNode root, int startValue, int destValue) {
+            TreeNode closestNode = dfs(root, startValue, destValue);
+            StringBuilder startPath = new StringBuilder();
+            StringBuilder destPath = new StringBuilder();
+            find(closestNode, startValue, startPath, true);
+            find(closestNode, destValue, destPath, false);
+            return startPath.append(destPath).toString();
+        }
+
+        private TreeNode dfs(TreeNode node, int startValue, int destValue) {
+            if (node == null) {
+                return null;
+            }
+            if (node.val == startValue) {
+                return node;
+            }
+            if (node.val == destValue) {
+                return node;
+            }
+            TreeNode left = dfs(node.left, startValue, destValue);
+            TreeNode right = dfs(node.right, startValue, destValue);
+            if (left != null && right != null) {
+                return node;
+            }
+            if (left == null && right == null) {
+                return null;
+            }
+            return left != null ? left : right;
+        }
+
+        private boolean find(TreeNode node, int value, StringBuilder path, boolean findStart) {
+            if (node == null) {
+                return false;
+            }
+            if (node.val == value) {
+                return true;
+            }
+            path.append(findStart ? "U" : "L");
+            boolean left = find(node.left, value, path, findStart);
+            if (left) {
+                return true;
+            }
+            path.deleteCharAt(path.length() - 1);
+            path.append(findStart ? "U" : "R");
+            boolean right = find(node.right, value, path, findStart);
+            if (right) {
+                return true;
+            }
+            path.deleteCharAt(path.length() - 1);
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
