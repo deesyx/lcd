ef4b77217ba0d12ca9f224a01c53c3ad2576c74c
xuanguang.hu <huxuanguang@hotmail.com>
2022-10-12 13:29:18 +0800

    20221012

---
+++ src/leetcode/editor/cn/LinkedListComponents_Eight17.java
@@ -0,0 +1,92 @@
+//给定链表头结点 head，该链表上的每个结点都有一个 唯一的整型值 。同时给定列表 nums，该列表是上述链表中整型值的一个子集。 
+//
+// 返回列表 nums 中组件的个数，这里对组件的定义为：链表中一段最长连续结点的值（该值必须在列表 nums 中）构成的集合。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入: head = [0,1,2,3], nums = [0,1,3]
+//输出: 2
+//解释: 链表中,0 和 1 是相连接的，且 nums 中不包含 2，所以 [0, 1] 是 nums 的一个组件，同理 [3] 也是一个组件，故返回 2。 
+//
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入: head = [0,1,2,3,4], nums = [0,3,1,4]
+//输出: 2
+//解释: 链表中，0 和 1 是相连接的，3 和 4 是相连接的，所以 [0, 1] 和 [3, 4] 是两个组件，故返回 2。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数为n 
+// 1 <= n <= 10⁴ 
+// 0 <= Node.val < n 
+// Node.val 中所有值 不同 
+// 1 <= nums.length <= n 
+// 0 <= nums[i] < n 
+// nums 中所有值 不同 
+// 
+// Related Topics 数组 哈希表 链表 👍 145 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListComponents_Eight17 {
+    public static void main(String[] args) {
+        Solution solution = new LinkedListComponents_Eight17().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public int numComponents(ListNode head, int[] nums) {
+            Set<Integer> m = new HashSet<>();
+            for (int n : nums) {
+                m.add(n);
+            }
+
+            int ans = 0;
+            int componentLength = 0;
+            ListNode node = head;
+            while (node != null) {
+                if (m.contains(node.val)) {
+                    componentLength++;
+                } else {
+                    if (componentLength != 0) {
+                        ans++;
+                    }
+                    componentLength = 0;
+                }
+                node = node.next;
+            }
+            if (componentLength != 0) {
+                ans++;
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
