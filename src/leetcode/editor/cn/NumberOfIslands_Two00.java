edf552ecba0b7838c79269250d46b4b082345890
xuanguang.hu <huxuanguang@hotmail.com>
2023-01-26 12:05:03 +0800

    20230126

---
+++ src/leetcode/editor/cn/NumberOfIslands_Two00.java
@@ -0,0 +1,94 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 2042 👎 0
+
+
+package leetcode.editor.cn;
+
+public class NumberOfIslands_Two00 {
+    public static void main(String[] args) {
+        Solution solution = new NumberOfIslands_Two00().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        private int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        private int m;
+        private int n;
+
+        public int numIslands(char[][] grid) {
+            m = grid.length;
+            n = grid[0].length;
+
+            int ans = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dfs(grid, i, j)) {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        private boolean dfs(char[][] grid, int i, int j) {
+            if (grid[i][j] != '1') {
+                return false;
+            }
+
+            grid[i][j] = '2';
+            for (int[] dir : dirs) {
+                int ni = i + dir[0];
+                int nj = j + dir[1];
+                if (ni < 0 || ni >= m || nj < 0 || nj >= n) {
+                    continue;
+                }
+                dfs(grid, ni, nj);
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
