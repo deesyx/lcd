6058d2126fd25f22be43b2224644bb33c73c08ae
huxuanguang <huxuanguang@microconnect.com>
2025-06-13 09:24:22 +0800

    11

--- src/leetcode/editor/cn/LongestCommonSubsequence_1143.java
+++ src/leetcode/editor/cn/LongestCommonSubsequence_1143.java
@@ -54,13 +54,17 @@ public class LongestCommonSubsequence_1143 {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * dp[i][j]表示text1[0...i]和text2[0...j]的最长公共子序列长度
+     */
     class Solution {
         public int longestCommonSubsequence(String text1, String text2) {
             int[][] dp = new int[text1.length() + 1][text2.length() + 1];
             for (int i = 1; i <= text1.length(); i++) {
                 for (int j = 1; j <= text2.length(); j++) {
                     if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                        dp[i][j] = dp[i - 1][j - 1] + 1;// 数组取text1.length() + 1的原因就是因为这里，这样可以实现统一处理
                     } else {
                         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                     }
