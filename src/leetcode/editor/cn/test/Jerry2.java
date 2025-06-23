205df6244cacee26f2b0380a63ca2b7a909a2342
huxuanguang <huxuanguang@microconnect.com>
2025-06-23 08:46:21 +0800

    144

--- src/leetcode/editor/cn/test/Jerry2.java
+++ src/leetcode/editor/cn/test/Jerry2.java
@@ -2,11 +2,6 @@ package leetcode.editor.cn.test;
 
 public class Jerry2 {
 
-    /**
-     * 数据结构 -- linkhashmap hashmap+双向链表
-     * 淘汰数据
-     *
-     */
     public static void main(String[] args) {
         System.out.println("hello world");
     }
