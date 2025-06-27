c29a92fd127a989452623dc31a364740a98ef466
huxuanguang <huxuanguang@microconnect.com>
2025-06-27 09:01:14 +0800

    357

---
+++ src/leetcode/editor/cn/CountNumbersWithUniqueDigits_357.java
@@ -0,0 +1,62 @@
+//给你一个整数 n ，统计并返回各位数字都不同的数字 x 的个数，其中 0 <= x < 10ⁿ 。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 2
+//输出：91
+//解释：答案应为除去 11、22、33、44、55、66、77、88、99 外，在 0 ≤ x < 100 范围内的所有数字。 
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 0
+//输出：1
+// 
+//
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 8 
+// 
+//
+// Related Topics 数学 动态规划 回溯 👍 362 👎 0
+
+
+package leetcode.editor.cn;
+
+public class CountNumbersWithUniqueDigits_357 {
+    public static void main(String[] args) {
+        Solution solution = new CountNumbersWithUniqueDigits_357().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int countNumbersWithUniqueDigits(int n) {
+            if (n == 0) {
+                return 1;
+            }
+
+            int ans = 10;
+            int base = 9;
+            for (int i = 0; i < n - 1; i++) {
+                base = base * (9 - i);
+                ans += base;
+            }
+
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
