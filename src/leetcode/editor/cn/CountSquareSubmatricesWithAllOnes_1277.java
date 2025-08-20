f0d727b2cb1649a3eccf5126b30a24b21c35e8e7
huxuanguang <huxuanguang@microconnect.com>
2025-08-20 09:12:27 +0800

    1277

---
+++ src/leetcode/editor/cn/CountSquareSubmatricesWithAllOnes_1277.java
@@ -0,0 +1,78 @@
+//给你一个 m * n 的矩阵，矩阵中的元素不是 0 就是 1，请你统计并返回其中完全由 1 组成的 正方形 子矩阵的个数。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：matrix =
+//[
+//  [0,1,1,1],
+//  [1,1,1,1],
+//  [0,1,1,1]
+//]
+//输出：15
+//解释： 
+//边长为 1 的正方形有 10 个。
+//边长为 2 的正方形有 4 个。
+//边长为 3 的正方形有 1 个。
+//正方形的总数 = 10 + 4 + 1 = 15.
+// 
+//
+// 示例 2： 
+//
+// 输入：matrix = 
+//[
+//  [1,0,1],
+//  [1,1,0],
+//  [1,1,0]
+//]
+//输出：7
+//解释：
+//边长为 1 的正方形有 6 个。 
+//边长为 2 的正方形有 1 个。
+//正方形的总数 = 6 + 1 = 7.
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr.length <= 300 
+// 1 <= arr[0].length <= 300 
+// 0 <= arr[i][j] <= 1 
+// 
+//
+// Related Topics 数组 动态规划 矩阵 👍 356 👎 0
+
+
+package leetcode.editor.cn;
+
+public class CountSquareSubmatricesWithAllOnes_1277 {
+    public static void main(String[] args) {
+        Solution solution = new CountSquareSubmatricesWithAllOnes_1277().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int countSquares(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int ans = 0;
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == 0) {
+                        continue;
+                    }
+
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i][j + 1], dp[i + 1][j]), dp[i][j]) + 1;
+                    ans += dp[i + 1][j + 1];
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
