836d80ee01e7f9d98bf20c17277213b9b28b3321
huxuanguang <huxuanguang@microconnect.com>
2025-08-27 09:13:50 +0800

    6

---
+++ src/leetcode/editor/cn/ZigzagConversion_6.java
@@ -0,0 +1,119 @@
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+//
+// 
+//P   A   H   N
+//A P L S I I G
+//Y   I   R 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// 
+//string convert(string s, int numRows); 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+// 
+//
+//示例 2：
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "A", numRows = 1
+//输出："A"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由英文字母（小写和大写）、',' 和 '.' 组成 
+// 1 <= numRows <= 1000 
+// 
+//
+// Related Topics 字符串 👍 2557 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigzagConversion_6 {
+    public static void main(String[] args) {
+        Solution solution = new ZigzagConversion_6().new Solution();
+        solution.convert("AB", 1);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String convert(String s, int numRows) {
+            List<StringBuilder> rows = new ArrayList<>();
+            for (int i = 0; i < numRows; i++) {
+                rows.add(new StringBuilder());
+            }
+            int idx = 0;
+            int row = 0;
+            boolean down = true;
+            while (idx < s.length()) {
+                if (down) {
+                    if (row <= numRows - 1) {
+                        rows.get(row).append(s.charAt(idx));
+                        if (row < numRows - 1) {
+                            row++;
+                            down = true;
+                        } else {
+                            if (row > 0) {
+                                row--;
+                            }
+                            down = false;
+                        }
+                    }
+                } else {
+                    if (row >= 0) {
+                        rows.get(row).append(s.charAt(idx));
+                        if (row > 0) {
+                            row--;
+                            down = false;
+                        } else {
+                            if (row < numRows - 1) {
+                                row++;
+                            }
+                            down = true;
+                        }
+                    }
+                }
+                idx++;
+            }
+
+            StringBuilder ans = new StringBuilder();
+            for (StringBuilder rowStr : rows) {
+                ans.append(rowStr);
+            }
+            return ans.toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
