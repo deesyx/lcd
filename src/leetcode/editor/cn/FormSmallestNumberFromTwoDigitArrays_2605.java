aac97fce67e2cfb23f93e1b4101925cc95da9a9d
huxuanguang <deesyx@qq.com>
2026-03-18 08:21:40 +0800

    2605

---
+++ src/leetcode/editor/cn/FormSmallestNumberFromTwoDigitArrays_2605.java
@@ -0,0 +1,61 @@
+//给你两个只包含 1 到 9 之间数字的数组 nums1 和 nums2 ，每个数组中的元素 互不相同 ，
+// 请你返回 最小 的数字，两个数组都 至少 包含这个数
+//字的某个数位。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums1 = [4,1,3], nums2 = [5,7]
+//输出：15
+//解释：数字 15 的数位 1 在 nums1 中出现，数位 5 在 nums2 中出现。15 是我们能得到的最小数字。
+// 
+//
+// 示例 2： 
+//
+// 输入：nums1 = [3,5,2,6], nums2 = [3,1,7]
+//输出：3
+//解释：数字 3 的数位 3 在两个数组中都出现了。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 9 
+// 1 <= nums1[i], nums2[i] <= 9 
+// 每个数组中，元素 互不相同 。 
+// 
+//
+// Related Topics 数组 哈希表 枚举 👍 101 👎 0
+
+
+package leetcode.editor.cn;
+
+public class FormSmallestNumberFromTwoDigitArrays_2605 {
+    public static void main(String[] args) {
+        Solution solution = new FormSmallestNumberFromTwoDigitArrays_2605().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minNumber(int[] nums1, int[] nums2) {
+            int ans = Integer.MAX_VALUE;
+            for (int i = 0; i < nums1.length; i++) {
+                for (int j = 0; j < nums2.length; j++) {
+                    if (nums1[i] == nums2[j]) {
+                        ans = Math.min(ans, nums1[i]);
+                    } else {
+                        int tmp = Math.min(nums1[i] * 10 + nums2[j], nums2[j] * 10 + nums1[i]);
+                        ans = Math.min(ans, tmp);
+                    }
+                }
+            }
+            return ans;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
